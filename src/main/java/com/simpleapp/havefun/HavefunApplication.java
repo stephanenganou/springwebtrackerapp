81960b6d9b922ccced82471bbef0b241f358fe80
Nganou Wafo, Stephane <stephane.nganou.wafo@ausy-technologies.de>
2021-10-14 19:37:27 +0200

    project initiation

---
+++ src/main/java/com/simpleapp/havefun/HavefunApplication.java
@@ -0,0 +1,13 @@
+package com.simpleapp.havefun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HavefunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HavefunApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/simpleapp/havefun/HavefunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simpleapp.havefun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HavefunApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
