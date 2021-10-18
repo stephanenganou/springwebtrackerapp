871d0692643cc6c7c13daa586c95a89e4a70afc3
Nganou Wafo, Stephane <stephane.nganou.wafo@ausy-technologies.de>
2021-10-18 17:04:31 +0200

    add login stuffs

---
+++ src/main/java/com/simpleapp/havefun/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.simpleapp.havefun.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+
+/**
+ * @author Stephane, Nganou Wafo
+ * Sets the configurations for the Security
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        // add out users for in Memory authentication
+
+        User.UserBuilder users = User.withDefaultPasswordEncoder();
+
+        auth.inMemoryAuthentication()
+                .withUser(users.username("stephane").password("stephane").roles("MANAGER"));
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/showMyLoginPage")
+                .loginProcessingUrl("/authenticateTheUser")
+                .failureUrl("/showMyLoginPage?error=true")
+                .permitAll();
+    }
+}
---
+++ src/main/java/com/simpleapp/havefun/web/controller/LoginController.java
@@ -0,0 +1,23 @@
+package com.simpleapp.havefun.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author Stephane, Nganou Wafo
+ * Handle the logs form process.
+ */
+@Controller
+public class LoginController {
+
+    @GetMapping("/showMyLoginPage")
+    public String showMyLoginPage(@RequestParam(value = "error", required = false) boolean isError,
+                                  Model theModel) {
+        if(isError){
+            theModel.addAttribute("loginError", true);
+        }
+        return "plain-login";
+    }
+}
